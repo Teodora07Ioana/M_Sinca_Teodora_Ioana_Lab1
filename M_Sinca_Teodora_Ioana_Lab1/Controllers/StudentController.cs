b9109735e24fa95b9638f90a94f0655d363b56df
40758 <40758@DESKTOP-U6HJMHG>
2024-11-24 23:26:01 +0200

    Metode in controller

---
+++ M_Sinca_Teodora_Ioana_Lab1/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace M_Sinca_Teodora_Ioana_Lab1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Metoda 1: Index - Mesaj de bun venit
+        public IActionResult Index()
+        {
+           
+            return Content("Bun venit la laboratorul nostru ASP.NET Core MVC!");
+        }
+
+        // Metoda 2: Salut utilizator
+        public IActionResult Salut()
+        {
+            return Content("Salut, draga student! Sper sa ai o zi minunata!");
+        }
+
+        // Metoda 3: Mesaj cu parametru string și întreg
+        public IActionResult Mesaj(string nume, int varsta)
+        {
+           
+            return Content($"Salut, {nume}! Ai {varsta} ani si esti binevenit la laborator!");
+        }
+    }
+}

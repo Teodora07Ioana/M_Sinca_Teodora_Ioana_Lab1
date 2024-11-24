2eab2937b0016c073552a4a66e88506704faf2c2
40758 <40758@DESKTOP-U6HJMHG>
2024-11-24 23:08:31 +0200

    Add project files.

---
+++ M_Sinca_Teodora_Ioana_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace M_Sinca_Teodora_Ioana_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
